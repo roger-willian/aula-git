307044b576ad5427d4e369fb347638b99e690f13
Giovani Signori <gigio.sig@gmail.com>
2022-09-08 13:49:00 -0300

    Teste Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,31 @@
+
+public class Funcionario {
+
+    private String nome;
+    private double salario;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
--- Teste.java
+++ Teste.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teste {
-    
+
+    private static List<Funcionario> criarFuncionarios() {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        funcionarios.add(new Funcionario("Cidcley", 1250.0));
+        funcionarios.add(new Funcionario("Adegilson", 2450.0));
+        funcionarios.add(new Funcionario("Evalnei", 4790.0));
+        return funcionarios;
+    }
+
+    private static double somarSalariosFuncionarios(List<Funcionario> funcionarios) {
+        return funcionarios.stream()
+            .map(funcionario -> funcionario.getSalario())
+            .reduce(0.0, Double::sum);
+    }
+
+    private static void printarNomesFuncionarios(List<Funcionario> funcionarios) {
+        System.out.println("\n=-=-=-= Funcionários =-=-=-=");
+        funcionarios.forEach(funcionario -> System.out.println(funcionario.getNome()));
+    }
+
     public static void main(String...args) {
-        System.out.println("...");
+        List<Funcionario> funcionarios = criarFuncionarios();
+
+        printarNomesFuncionarios(funcionarios);
+        double somaSalarios = somarSalariosFuncionarios(funcionarios);
+
+        System.out.format("\nSoma dos salários: R$ %.2f\n", somaSalarios);
     }
 
 }                           
